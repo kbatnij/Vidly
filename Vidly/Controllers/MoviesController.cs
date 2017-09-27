14aace79fa98dd84bc5b2ae42060eac7cd511f89
Khaldon Batnij <batnij.k@gmail.com>
2017-09-27 18:24:06 -0500

    * Added routing to movies * Changed default Bootstrap theme to Bootstrap-lumen * Added ViewModel for Movies and Customers.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Customer1"},
+                new Customer{ Name = "Customer2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "/" + month);
+        }
+    }
+}
