731d640859462589795b86718744d350ff4a0873
Khaldon Batnij <batnij.k@gmail.com>
2017-09-28 19:56:39 -0500

    Commit app before Excercise 3

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -22,31 +22,68 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        public ViewResult New()
-        {
-            var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel()
-            {
-                MembershipTypes = membershipTypes
-            };
-            return View(viewModel);
-        }
-
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(s=> s.MembershipType).ToList();
+            var customers = _context.Customers.Include(s => s.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int Id)
         {
-            var customer = _context.Customers.Include(s=> s.MembershipType).SingleOrDefault(s => s.Id == Id);
+            var customer = _context.Customers.Include(s => s.MembershipType).SingleOrDefault(s => s.Id == Id);
             if (customer == null)
             {
                 return HttpNotFound();
             }
             return View(customer);
+        }
+
+        public ViewResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(s => s.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.Birthdate = customer.Birthdate;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(s => s.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
     }
 }
